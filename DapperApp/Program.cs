6d210dcc95cc5f19fcbbad713a16621481879a51
MaryTwist <isterika-1989@bk.ru>
2019-04-24 23:48:07 +0300

    Реализация: CLI и репозиторий с использованием Dapper.

---
+++ DapperApp/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DapperApp
+{
+    class Program
+    {
+        private static readonly string _connectionString =
+            @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=stuff;Integrated Security=True";
+
+        private static readonly char[] spaceSplitter = new char[] { ' ' };
+        private static readonly Dictionary<string, Action<string[]>> _commandHandlers;
+        private static readonly Repositories.ItemsRepository _items;
+        private static bool _terminate;
+
+        static Program()
+        {
+            _terminate = false;
+
+            _items = new Repositories.ItemsRepository(_connectionString);
+
+            _commandHandlers = new Dictionary<string, Action<string[]>>
+            {
+                { "help", HelpHandler },
+                { "add", AddHandler },
+                { "select", SelectHandler },
+                { "update", UpdateHandler },
+                { "delete", DeleteHandler },
+                { "quit", QuitHandler },
+                { "exit", QuitHandler }
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            HelpHandler();
+
+            while (true)
+            {
+                var arr = Console.ReadLine().Split(spaceSplitter, StringSplitOptions.RemoveEmptyEntries);
+
+                try
+                {
+                    HandleInput(arr);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                if (_terminate)
+                {
+                    break;
+                }
+            }
+        }
+
+        private static void HelpHandler(string[] args = null)
+        {
+            Console.WriteLine("Commands:");
+
+            foreach(var item in _commandHandlers)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+
+        private static void HandleInput(string[] args)
+        {
+            if (_commandHandlers.ContainsKey(args[0]))
+            {
+                _commandHandlers[args[0]](args);
+            }
+            else
+            {
+                Console.WriteLine("Uknown command");
+            }
+        }
+
+        private static void AddHandler(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Missing argument");
+                return;
+            }
+
+            var id = _items.Add(args[1]);
+
+            Console.WriteLine("New item id: {0}", id);
+        }
+
+        private static void QuitHandler(string[] args = null)
+        {
+            _terminate = true;
+
+            Console.WriteLine("Bye!");
+        }
+
+        private static void SelectHandler(string[] args)
+        {
+            var items = new List<Models.Item>();
+
+            if (args.Length == 1)
+            {
+                items = _items.Select();
+            }
+            else
+            {
+                if (args[1] == "name")
+                {
+                    items.Add(_items.Select(args[2]));
+                }
+                else if (args[1] == "id")
+                {
+                    items.Add(_items.Select(int.Parse(args[2])));
+                }
+                else
+                {
+                    Console.WriteLine("Uknown selector");
+                    return;
+                }
+            }
+
+            foreach(var item in items)
+            {
+                Console.WriteLine("{0}: {1}", item.Id, item.Name);
+            }
+        }
+
+        private static void DeleteHandler(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Missing argument");
+                return;
+            }
+
+            _items.Delete(int.Parse(args[1]));
+
+            Console.WriteLine("Deleted");
+        }
+
+        private static void UpdateHandler(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Missing arguments");
+                return;
+            }
+
+            _items.Update(int.Parse(args[1]), args[2]);
+
+            Console.WriteLine("Updated");
+        }
+    }
+}
---
+++ DapperApp/Repositories/ItemsRepository.cs
@@ -0,0 +1,87 @@
+﻿using Dapper;
+using DapperApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace DapperApp.Repositories
+{
+    public class ItemsRepository
+    {
+        private readonly string _connectionString;
+
+        public ItemsRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public int Add(string Name)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                return conn
+                    .Query<int>(
+                        @"insert into dbo.Items(Name) values (@name);
+                          select cast(scope_identity() as int);",
+                        new { name = Name })
+                    .First();
+            }
+        }
+
+        public List<Item> Select()
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                return conn
+                    .Query<Item>(@"select Id, Name from dbo.Items;")
+                    .ToList();
+            }
+        }
+
+        public Item Select(int id)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                return conn
+                    .Query<Item>(
+                        @"select Id, Name from dbo.Items where Id = @id;",
+                        new { id })
+                    .Single();
+            }
+        }
+
+        public Item Select(string name)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                return conn
+                    .Query<Item>(
+                        @"select Id, Name from dbo.Items where Name = @name;",
+                        new { name })
+                    .Single();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                conn.Execute(
+                    @"delete from dbo.Items where Id = @id;",
+                    new { id });
+            }
+        }
+
+        public void Update(int id, string name)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                conn.Execute(
+                    @"update dbo.Items set Name = @name where Id = @id;",
+                    new { id, name });
+            }
+        }
+    }
+}
